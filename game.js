fa80b34202f7430a6300875e6ec32f1b28f7a1bd
paulam92 <szakacsmpaula@gmail.com>
2021-07-02 20:08:16 +0200

    first commit

---
+++ game.js
@@ -0,0 +1,16 @@
+kaboom({
+    global: true,
+    fullscreen: true,
+    scale: 1,
+    debug: true,
+    clearColor: [0, 0, 0, 1],
+})
+loadRoot('https://i.imgur.com/')
+loadSprite('coin', 'wbKxhcd.png')
+loadSprite('evil-shroom', 'wbKxhcd.png')
+
+scene("game", () => {
+ layers(['bg', 'obj', 'ui'], 'obj')
+})
+
+start("game")
