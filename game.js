b8e0e1a51d26c9355a135f3da079c821af39eddc
paulam92 <szakacsmpaula@gmail.com>
2021-07-02 22:02:27 +0200

    get scenes

--- game.js
+++ game.js
@@ -1,16 +1,68 @@
 kaboom({
     global: true,
     fullscreen: true,
-    scale: 1,
+    scale: 2,
     debug: true,
     clearColor: [0, 0, 0, 1],
-})
-loadRoot('https://i.imgur.com/')
-loadSprite('coin', 'wbKxhcd.png')
-loadSprite('evil-shroom', 'wbKxhcd.png')
-
-scene("game", () => {
- layers(['bg', 'obj', 'ui'], 'obj')
-})
-
-start("game")
+  })
+  
+  const MOVE_SPEED = 120
+  const JUMP_FORCE = 360
+  const BIG_JUMP_FORCE = 550
+  let CURRENT_JUMP_FORCE = JUMP_FORCE
+  let isJumping = true
+  const FALL_DEATH = 400
+  
+  loadRoot('https://i.imgur.com/')
+  loadSprite('coin', 'wbKxhcd.png')
+  loadSprite('evil-shroom', 'KPO3fR9.png')
+  loadSprite('brick', 'pogC9x5.png')
+  loadSprite('block', 'M6rwarW.png')
+  loadSprite('mario', 'Wb1qfhK.png')
+  loadSprite('mushroom', '0wMd92p.png')
+  loadSprite('surprise', 'gesQ1KP.png')
+  loadSprite('unboxed', 'bdrLpi6.png')
+  loadSprite('pipe-top-left', 'ReTPiWY.png')
+  loadSprite('pipe-top-right', 'hj2GK4n.png')
+  loadSprite('pipe-bottom-left', 'c1cYSbt.png')
+  loadSprite('pipe-bottom-right', 'nqQ79eI.png')
+  
+  loadSprite('blue-block', 'fVscIbn.png')
+  loadSprite('blue-brick', '3e5YRQd.png')
+  loadSprite('blue-steel', 'gqVoI2b.png')
+  loadSprite('blue-evil-shroom', 'SvV4ueD.png')
+  loadSprite('blue-surprise', 'RMqCc1G.png')
+  
+  
+  
+  scene("game", ({ level, score }) => {
+    layers(['bg', 'obj', 'ui'], 'obj')
+  
+    const maps = [
+      [
+        '                                      ',
+        '                                      ',
+        '                                      ',
+        '                                      ',
+        '                                      ',
+        '     %   =*=%=                        ',
+        '                                      ',
+        '                            -+        ',
+        '                    ^   ^   ()        ',
+        '==============================   =====',
+      ],
+      [
+        '£                                       £',
+        '£                                       £',
+        '£                                       £',
+        '£                                       £',
+        '£                                       £',
+        '£        @@@@@@              x x        £',
+        '£                          x x x        £',
+        '£                        x x x x  x   -+£',
+        '£               z   z  x x x x x  x   ()£',
+        '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',
+      ]
+    ]
+  
+ 
